689c9cb74b1b6626222feba9409259735fc20896
Tamara Ezveđ <ezvedtamara@gmail.com>
2026-03-06 18:47:14 +0100

    Add ErrorBoundary import to index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import ErrorBoundary from './ErrorBoundary';   // 👈 add this
+import ErrorBoundary from './ErrorBoundary';   
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
